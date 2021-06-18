154f8322b22182addf965199140a39b045e71653
machine0318 <58125633+machine0318@users.noreply.github.com>
2021-06-18 09:02:00 -0400

    Final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  btnClickCount = [];
+  btnClick: number = 0;
+
+  onButtonClick() {
+    this.btnClick += 1;
+    this.btnClickCount.push(this.btnClick);
+  }
+
+  getClick() {
+    return this.btnClick % 2 == 0 ? 'hidden' : 'visible';
+  }
+  
+  getColor(btnClick) {
+    return this.btnClick > 4 ? 'blue' : 'white';
+}
+
 }
